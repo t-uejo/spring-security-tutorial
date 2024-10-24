4cee3c7f21fd85c1532af358b014e708e91adaab
t-uejo <tiki.ur.tiki@icloud.com>
2024-10-24 22:32:13 +0900

    feature：SecurityFilterChainをBean登録し、loginページ以外を認証が必要なページとした

---
+++ src/main/java/com/example/its/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.example.its.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http
+    ) throws Exception {
+
+        http
+                .formLogin(login -> login.loginPage("/login"))
+                .authorizeHttpRequests(authorize -> authorize
+                                .requestMatchers(new AntPathRequestMatcher("/login/**")).permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/**")).authenticated()
+                )
+                ;
+
+        return http.build();
+    }
+}
